b42c3d4daeca4188243bc6b16006d1478922b20b
idsc3101 <idsc3101@gmail.com>
2013-10-07 15:20:22 -0500

    Junghun Kwon's Daywork 5

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -5,11 +5,26 @@ Public Class Form1
     Private Sub btnDetermine_Click(sender As System.Object, e As System.EventArgs) Handles btnDetermine.Click
         Dim name As String
         name = txtFullName.Text
+
+        Dim result As String
+        result = FirstName(name)
         txtFirstName.Text = FirstName(name)
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+        'find the first space
+        Dim idx As Integer
+        idx = name.IndexOf(" ")
+
+        'extract the first name
+        Dim fName As String
+        fName = name.Substring(0, idx)
+
+        Return fName
+
     End Function
 
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -4,8 +4,26 @@ Public Class Form1
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
 
-        txtHyp.Text = "Not Implemented Yet"
+        Dim x As Double = CDbl(txtSideOne.Text)
+        Dim y As Double = CDbl(txtSideTwo.Text)
+        Dim z As Double
+
+        z = Hypotenuse(x, y)
+
+        txtHyp.Text = CStr(z)
 
     End Sub
 
+        Function Hypotenuse(Byval a as double, Byval b as double) as double
+        Dim c As Double
+        c = Math.Sqrt(a ^ 2 + b ^ 2)
+
+        Return c
+    End Function
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
+
+
